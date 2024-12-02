a183a7dc670621f039ca6ba033382300fa13d3ec
AlexandreVlt <140422265+AlexandreVlt@users.noreply.github.com>
2024-12-02 15:45:37 +0100

    Revert "add"

--- app/models/movie_event.rb
+++ app/models/movie_event.rb
@@ -1,8 +1,6 @@
 class MovieEvent < ApplicationRecord
   belongs_to :event
-  belongs_to :movie, optional: true
+  belongs_to :movie
+  has_many :votes
   has_many :movie_comments
-  has_one :vote
-
-  has_one :user, through: :vote
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,5 @@
 class Vote < ApplicationRecord
   belongs_to :movie_event
   belongs_to :user
-
-  # event, fait référence à l'event en cours
-  has_one :event, -> { where movie_events: { selected_movie: true } }, through: :movie_event
-  # movie, fait référence au film choisi dans l'event en cours
-  has_one :movie, -> { where movie_events: { selected_movie: true } }, through: :movie_event
+  has_many :events, through: :movie_events
 end
